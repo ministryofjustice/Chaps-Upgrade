091114f6b589ca9218c9ee8f2537e5499513f0ff
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-11-19 10:58:16 +0000

    amend bootstrap.ps1 logfile destination

--- Chaps-Upgrade/bootstrap.ps1
+++ Chaps-Upgrade/bootstrap.ps1
@@ -11,12 +11,39 @@ foreach($key in [System.Environment]::GetEnvironmentVariables('Process').Keys) {
 }
 
 # restart IIS
+Write_Host "Restarting IIS..."
 iisreset
 
-# echo the IIS log to the console:
-Write-Host 'Start W3SVC'
+# Start W3SVC service
+Write_Host "Starting W3SVC service"
 Start-Service W3SVC
+
+# Send a request to localhost to trigger logging
+Write-Host "Sending request to localhost to generate IIS logs..."
 Invoke-WebRequest http://localhost -UseBasicParsing | Out-Null
-netsh http flush logbuffer | Out-Null
-Write-Host 'Send IIS logs to stdout:'
-Get-Content -path 'c:\inetpub\logs\logfiles\W3SVC\u_extend1.log' -Tail 1 -Wait
+
+# Automatically detect IIS log file path
+Write-Host "Detecting IIS log directory..."
+$logDirectory = Get-WebConfigurationProperty -Filter "system.applicationHost/sites/siteDefaults/logFile" -Name "directory"
+$logDirectory = if ($logDirectory) { $logDirectory } else { "C:\inetpub\logs\logfiles" }
+
+# Determine site ID (assumes single site)
+$siteID = Get-WebConfigurationProperty -Filter "system.applicationHost/sites/site[@name='Default Web Site']" -Name "id"
+$logPath = Join-Path -Path $logDirectory -ChildPath "W3SVC$siteID\u_extend1.log"
+
+Write-Host "Log file path: $logPath"
+
+# Wait for the log file to be created, with retries
+$retries = 10
+while (!(Test-Path -Path $logPath) -and ($retries -gt 0)) {
+    Write-Host "Log file not found, waiting for 5 seconds..."
+    Start-Sleep -Seconds 5
+    $retries--
+}
+
+if (Test-Path -Path $logPath) {
+    Write-Host "Log file found. Streaming contents to stdout:"
+    Get-Content -Path $logPath -Tail 1 -Wait
+} else {
+    Write-Host "Log file still not found after retries. Ensure IIS is properly configured and traffic is reaching the site."
+}
